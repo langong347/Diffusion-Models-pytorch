982bb325c25ff4101f282370e36997c75ef94ea5
langong <lan.gong347@gmail.com>
2024-09-01 19:13:00 -0400

    Add util to organize cifar dir

--- utils.py
+++ utils.py
@@ -41,3 +41,23 @@ def setup_logging(run_name):
     os.makedirs(os.path.join("models", run_name), exist_ok=True)
     os.makedirs(os.path.join("results", run_name), exist_ok=True)
 
+
+def get_cifar10():
+    from torchvision.datasets import CIFAR10
+
+    root_dir = r"/Users/langong/Projects/diffusion/datasets/cifar10"
+    train_dir =  os.path.join(root_dir, "train")
+    test_dir = os.path.join(root_dir, "test")
+    trainset = CIFAR10(root=root_dir, train=True, download=True)
+    testset = CIFAR10(root=root_dir, train=False, download=True)
+
+    # Re-organize the downloaded image binaries to directory format suitable for ImageFolder
+    def save_images(dataset, data_dir):
+        for idx, (img, label) in enumerate(dataset):
+            label_dir = os.path.join(data_dir, str(label))
+            os.makedirs(label_dir, exist_ok=True)
+            img_path = os.path.join(label_dir, f"{idx}.png")
+            img.save(img_path)
+
+    save_images(trainset, train_dir)
+    save_images(testset, test_dir)
